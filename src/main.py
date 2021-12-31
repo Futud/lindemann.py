a750a9e6e4a703f3b2cba1ef1f76c9dff6d61b25
Futuy <futuyyy@protonmail.com>
2021-12-31 20:51:16 +0100

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import discord
+import random
+from discord import client
+messages = ["https://media.discordapp.net/attachments/922478840356425758/926507839982305340/20211220_162425.jpg", "https://media.discordapp.net/attachments/922478840356425758/926507840221356102/20211220_205819.jpg"]
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user.name}")
+
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='nach dem rechten'))
+
+    async def on_message(self, message):
+        if client.user.mentioned_in(message):
+            await message.channel.send(random.choice(messages))
+
+client = MyClient()
+client.run("token")
